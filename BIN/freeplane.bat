9bf6fa78b69e13aa2f4d1ba0b4017ec9e997b110
macmarrum <7197131+macmarrum@users.noreply.github.com>
2024-04-07 21:58:02 +0200

    1.10.7-7

--- BIN/freeplane.bat
+++ BIN/freeplane.bat
@@ -1,5 +1,6 @@
 java ^
 --add-exports=java.desktop/sun.awt.shell=ALL-UNNAMED ^
 --add-exports=java.desktop/com.sun.java.swing.plaf.windows=ALL-UNNAMED ^
--Xmx512m -Dorg.freeplane.userfpdir="%APPDATA%\\Freeplane" ^
+-Xmx1g -Dorg.freeplane.userfpdir="%APPDATA%\\Freeplane" ^
+-Dfile.encoding=UTF-8 ^
 -jar freeplanelauncher.jar %*
